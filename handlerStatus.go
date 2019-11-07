f64ab32ac09224bf664b316fe565a5f3dd0866e8
hkhoel <hanskhoe@stud.ntnu.no>
2019-11-07 13:50:08 +0100

    Added total recipes and total ingredients to status, and commentet status

--- handlerStatus.go
+++ handlerStatus.go
@@ -8,34 +8,53 @@ import (
 
 // Status struct for status endpoint
 type Status struct {
-	Edemam   int     `json:"edemam"`
-	Database int     `json:"database"`
-	Uptime   float64 `json:"uptime"`
-	Version  string  `json:"version"`
+	Edemam           int     `json:"edemam"`
+	Database         int     `json:"database"`
+	TotalRecipe      int     `json:"total recipes"`
+	TotalIngredients int     `json:"total ingredients"`
+	Uptime           float64 `json:"uptime"`
+	Version          string  `json:"version"`
 }
 
 // HandlerStatus handles the status endpoint
 func HandlerStatus(w http.ResponseWriter, r *http.Request) {
 	var S Status
 
-	resp, err := http.Get("https://api.edamam.com/api/nutrition-details")
+	// Sets status for Edemam ***************************************
+	resp, err := http.Get("https://api.edamam.com/api/nutrition-details") // gets api
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	S.Edemam = resp.StatusCode
+	S.Edemam = resp.StatusCode // sets status code for api
 
-	resp, err = http.Get("https://firebase.google.com")
+	// Sets staus for database ***************************************
+	resp, err = http.Get("https://firebase.google.com") // gets link
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	S.Database = resp.StatusCode
+	S.Database = resp.StatusCode // sets status code for link
 
-	elapse := time.Since(StartTime)
-	S.Uptime = elapse.Seconds()
+	// Sets total of recipes *****************************************
+	statusRecipe, err := DBReadAllRecipes() // gets all recipes from database
+	if err != nil {
+		http.Error(w, "Could not retrieve collection "+RecipeCollection+" "+err.Error(), http.StatusInternalServerError)
+	}
+	S.TotalRecipe = len(statusRecipe) // sets totat for recipes
+
+	// Sets status for Ingredients ***********************************
+	statusIngredients, err := DBReadAllIngredients() // gets all ingredients from database
+	if err != nil {
+		http.Error(w, "Could not retrieve collection "+IngredientCollection+" "+err.Error(), http.StatusInternalServerError)
+	}
+	S.TotalIngredients = len(statusIngredients) // sers total for ingredients
+
+	// Sets status for uptime ****************************************
+	elapse := time.Since(StartTime) //sets run time
+	S.Uptime = elapse.Seconds()     //convert run time to seconds
 
 	S.Version = "v1"
 
