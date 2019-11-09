6ad49f752e5323c551d71ede6fa9fd0070499db1
hkhoel <hanskhoe@stud.ntnu.no>
2019-11-09 17:18:16 +0100

    commented firebase_test file

--- firebase_test.go
+++ firebase_test.go
@@ -17,29 +17,29 @@ func TestFirebase(t *testing.T) {
 	// testing of Ingredients functions for database **************
 	i := Ingredient{Name: "TestIngredient"}
 
-	err = DBSaveIngredient(&i)
+	err = DBSaveIngredient(&i) //test sevaing Ingredient
 	if err != nil {
 		t.Error(err)
 	}
 
 	var ingredients []Ingredient
-	ingredients, err = DBReadAllIngredients()
+	ingredients, err = DBReadAllIngredients() // test read all ingredients
 	if err != nil {
 		t.Error(err)
 	}
 
-	_, err = DBReadIngredientByID(ingredients[1].ID)
+	_, err = DBReadIngredientByID(ingredients[1].ID) // test read ingrediants by id
 	if err != nil {
 		t.Error(err)
 	}
 
-	i2, err := DBReadIngredientByName(i.Name)
+	i2, err := DBReadIngredientByName(i.Name) // test read ingrediants by name
 	if err != nil {
 		t.Error(err)
 	}
 
 	fmt.Println("test ingredient", i2)
-	err = DBDelete(i2.ID, IngredientCollection)
+	err = DBDelete(i2.ID, IngredientCollection) // test delete and delet test ingredieant
 	if err != nil {
 		t.Error(err)
 	}
@@ -48,29 +48,29 @@ func TestFirebase(t *testing.T) {
 
 	r := Recipe{RecipeName: "TestRecipe"}
 
-	err = DBSaveRecipe(&r)
+	err = DBSaveRecipe(&r) // test saving recipe
 	if err != nil {
 		t.Error(err)
 	}
 
 	var Recipes []Recipe
-	Recipes, err = DBReadAllRecipes()
+	Recipes, err = DBReadAllRecipes() // test reading all recipes
 	if err != nil {
 		t.Error(err)
 	}
 
-	_, err = DBReadRecipeByID(Recipes[1].ID)
+	_, err = DBReadRecipeByID(Recipes[1].ID) // test reading recipe by id
 	if err != nil {
 		t.Error(err)
 	}
 
-	r2, err := DBReadRecipeByName(r.RecipeName)
+	r2, err := DBReadRecipeByName(r.RecipeName) // test reading recipe by name
 	if err != nil {
 		t.Error(err)
 	}
 
 	fmt.Println("test recipe ", r2)
-	err = DBDelete(r2.ID, RecipeCollection)
+	err = DBDelete(r2.ID, RecipeCollection) // Delets test recipe
 	if err != nil {
 		t.Error(err)
 	}
@@ -78,19 +78,19 @@ func TestFirebase(t *testing.T) {
 	// testing of Webhooks functions for database *****************************
 	w := Webhook{Event: "Test", URL: "www.Test.no", Time: time.Now()}
 
-	err = DBSaveWebhook(&w)
+	err = DBSaveWebhook(&w) // test saving webhook
 	if err != nil {
 		t.Error(err)
 	}
 
 	var wh []Webhook
-	wh, err = DBReadAllWebhooks()
+	wh, err = DBReadAllWebhooks() // test reading all webhooks
 	if err != nil {
 		t.Error(err)
 	}
 
 	fmt.Println("test webhooks ", wh[1].ID)
-	err = DBDelete(wh[1].ID, WebhooksCollection)
+	err = DBDelete(wh[1].ID, WebhooksCollection) // delets test webhook
 	if err != nil {
 		t.Error(err)
 	}
