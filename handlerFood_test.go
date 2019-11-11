9733ac9da376899af82a92fd49f3996fe38bcc46
hkhoel <hanskhoe@stud.ntnu.no>
2019-11-11 23:27:23 +0100

    created test for GetAllRecipes funk

--- handlerFood_test.go
+++ handlerFood_test.go
@@ -1,1 +1,22 @@
 package cravings
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetAllRecipes(t *testing.T) {
+
+	r, err := http.NewRequest("GET", "/cravings/food/", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	testRecipe := GetAllRecipes(w, r)
+	if len(testRecipe) == 0 {
+		t.Error("Cant read recipes from database")
+	}
+}
