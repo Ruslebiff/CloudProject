3ddde1919d88fb94ebff1c7d4d485a9133e54553
hkhoel <hanskhoe@stud.ntnu.no>
2019-11-11 23:44:12 +0100

    created test for handlerStatus

--- handlerStatus_test.go
+++ handlerStatus_test.go
@@ -1,7 +1,27 @@
 package cravings
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestHandlerStatus(t *testing.T) {
 
+	r, err := http.NewRequest("GET", "/cravings/status/", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	w := httptest.NewRecorder()
+	handler := http.HandlerFunc(HandlerStatus)
+	handler.ServeHTTP(w, r)
+
+	resp := w.Result()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Error(resp.StatusCode)
+	}
+	fmt.Println("test handlerStatus")
 }
