b465d63d42548eef85b4cc887ab87474e239f6d1
hkhoel <hanskhoe@stud.ntnu.no>
2019-11-12 00:03:14 +0100

    created part of the test for handlerMeal Get method

--- handlerMeal_test.go
+++ handlerMeal_test.go
@@ -1,7 +1,26 @@
 package cravings
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestHandlerMeal(t *testing.T) {
 
+	r, err := http.NewRequest("GET", "/cravings/meal/", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	w := httptest.NewRecorder()
+	handler := http.HandlerFunc(HandlerMeal)
+	handler.ServeHTTP(w, r)
+
+	resp := w.Result()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Error(resp.StatusCode)
+	}
+	fmt.Println("testeing handlerMeal GET method")
 }
