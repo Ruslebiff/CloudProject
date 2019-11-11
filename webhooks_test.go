122d6b2cf91e4ad7ac0343270e1985c9787721d6
hkhoel <hanskhoe@stud.ntnu.no>
2019-11-11 22:57:29 +0100

    commented out test code for delete method webhooks, because it is not done

--- webhooks_test.go
+++ webhooks_test.go
@@ -49,4 +49,40 @@ func TestHandlerWebhooks(t *testing.T) {
 
 	// Test Delete method for endpoint /cravings/webhooks/ ****************
 
+	// testWebhook := Webhook{Event: "TestWebhookDelete", URL: "www.Test.no", Time: time.Now()}
+	// testWebhook2 := Webhook{}
+
+	// err = DBSaveWebhook(&testWebhook) // test saving webhook
+	// if err != nil {
+	// 	t.Error(err)
+	// }
+
+	// test, err := DBReadAllWebhooks()
+	// if err != nil {
+	// 	t.Error(err)
+	// }
+
+	// for i := range test {
+	// 	if test[i].Event == testWebhook.Event {
+	// 		testWebhook2 = test[i]
+	// 	}
+	// }
+
+	// fmt.Println("testWebhook2: ", testWebhook2)
+
+	// r3, err := http.NewRequest("DELETE", "/cravings/webhooks/", nil)
+	// if err != nil {
+	// 	t.Error(err)
+	// }
+
+	// w3 := httptest.NewRecorder()
+	// handler3 := http.HandlerFunc(HandlerWebhooks)
+	// handler3.ServeHTTP(w3, r3)
+
+	// resp3 := w3.Result()
+
+	// if resp3.StatusCode != http.StatusOK {
+	// 	t.Error(resp3.StatusCode)
+	// }
+
 }
