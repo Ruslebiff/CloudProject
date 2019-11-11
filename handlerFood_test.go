8ab873daf9041aebd4c9ebabb7142e78fd0dad97
hkhoel <hanskhoe@stud.ntnu.no>
2019-11-11 23:32:51 +0100

    created test for GetAllIngredients func

--- handlerFood_test.go
+++ handlerFood_test.go
@@ -20,3 +20,19 @@ func TestGetAllRecipes(t *testing.T) {
 		t.Error("Cant read recipes from database")
 	}
 }
+
+func TestGetAllIngredients(t *testing.T) {
+
+	r, err := http.NewRequest("GET", "/cravings/food/", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	testRecipe := GetAllIngredients(w, r)
+	if len(testRecipe) == 0 {
+		t.Error("Cant read ingredients from database")
+	}
+
+}
