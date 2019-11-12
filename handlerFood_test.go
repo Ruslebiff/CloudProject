974c4fdff908f9211185c40ccb92dedbee6d2882
hkhoel <hanskhoe@stud.ntnu.no>
2019-11-12 12:10:02 +0100

    set up read token from file, and testing POST method for Ingredient

--- handlerFood_test.go
+++ handlerFood_test.go
@@ -1,10 +1,64 @@
 package cravings
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
+
+type testIngredient struct {
+	Token string `json:"token"`
+	Name  string `json:"name"`
+	Unit  string `json:"unit"`
+}
+
+func TestHandlerFood(t *testing.T) {
+
+	// Reads token from text file
+
+	var testToken string
+	file, err := os.Open("testToken.txt") // opens text file
+	if err != nil {
+		fmt.Println("Can't open file: ", err)
+	}
+
+	defer file.Close() // close file at the end
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		testToken = scanner.Text()
+	}
+
+	fmt.Println(testToken)
+
+	// Testing methon POST ***********************************************
+	i := testIngredient{Token: testToken, Name: "turmeric", Unit: "g"}
+	requestI, _ := json.Marshal(i)
+	requestTest := bytes.NewReader(requestI)
+	r, err := http.NewRequest("POST", "/cravings/food/ingredient", requestTest)
+	//r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		t.Error(err)
+	}
+	w := httptest.NewRecorder()
+	handler := http.HandlerFunc(HandlerFood)
+	handler.ServeHTTP(w, r)
+
+	resp := w.Result()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Error(resp.StatusCode)
+	}
+	fmt.Println("testeing handlerFood POST method")
+
+	//Testing method POST ***********************************************
+}
 
 func TestGetAllRecipes(t *testing.T) {
 
