cc068139e7a0bb592b519c244ba7767c33bfff29
hkhoel <hanskhoe@stud.ntnu.no>
2019-11-18 15:39:24 +0100

    reorganized and removed duplication code in webhook test

--- handlerWebhooks_test.go
+++ handlerWebhooks_test.go
@@ -9,90 +9,37 @@ import (
 	"testing"
 )
 
-// func TestHandlerWebhooks
+func TestHandlerWebhooks(t *testing.T) {
 
-func TestHandlerWebhooksPost(t *testing.T) {
-	// Test Post method for endpoint /cravings/webhooks/ ******************'
+	Post := "POST"
+	Get := "GET"
+	Delete := "DELETE"
+	URL := "/cravings/webhooks/"
+
 	webH := Webhook{Event: "testevent", URL: "www.testurl.com"} // create a webhook with event and url to send as body
-	req, _ := json.Marshal(webH)
-	reqTest := bytes.NewReader(req)                                   // convert struct to *Reader
-	r, err := http.NewRequest("POST", "/cravings/webhooks/", reqTest) // creats request with body
+	s := Webhook{}
 
-	if err != nil {
-		t.Error(err)
-	}
-
-	w := httptest.NewRecorder() // creates ResponseRecorder
-
-	handler := http.HandlerFunc(HandlerWebhooks) // test handlerWebhooks
-	handler.ServeHTTP(w, r)
-
-	resp := w.Result()
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK { // check that test went ok
-		t.Error(resp.StatusCode)
-	}
-
+	// Test Post method for endpoint /cravings/webhooks/
 	fmt.Println("testeing webhooks POST method")
-}
 
-func TestHandlerWebhooksGetA(t *testing.T) {
-	// Test Get method for endpoint /cravings/webhooks/ ***************************
-	r, err := http.NewRequest("GET", "/cravings/webhooks/", nil) //creates request
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	w := httptest.NewRecorder() // creates ResponseRecorder
-
-	handler := http.HandlerFunc(HandlerWebhooks) // test handlerWebhooks
-	handler.ServeHTTP(w, r)
-
-	resp := w.Result()
-	defer resp.Body.Close()
+	resp := ALLMethodWebhook(Post, URL, webH, t)
 
 	if resp.StatusCode != http.StatusOK { // check that test went ok
 		t.Error(resp.StatusCode)
 	}
 
+	// Test Get method for endpoint /cravings/webhooks/
 	fmt.Println("testing webhooks GET method for all webhooks")
-}
 
-func TestHandlerWebhooksGetO(t *testing.T) {
-	// Test Get method for endpoint /cravings/webhooks/ID **************
-	w := httptest.NewRecorder() // creates ResponseRecorder
-
-	wh, err := DBReadAllWebhooks(w) // reads all webhooks from database
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	r, err := http.NewRequest("GET", "/cravings/webhooks/"+wh[1].ID, nil) // creats request
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	w = httptest.NewRecorder() // creates ResponseRecorder
-
-	handler := http.HandlerFunc(HandlerWebhooks) // test handlerWebhooks
-	handler.ServeHTTP(w, r)
-
-	resp := w.Result()
-	defer resp.Body.Close()
+	resp = ALLMethodWebhook(Get, URL, s, t)
 
 	if resp.StatusCode != http.StatusOK { // check that test went ok
 		t.Error(resp.StatusCode)
 	}
 
+	// Test Get method for endpoint /cravings/webhooks/ID
 	fmt.Println("testing webhooks GET method for one webhook")
-}
 
-func TestHandlerWebhooksDelete(t *testing.T) {
-	// Test Delete method for endpoint /cravings/webhooks/ ****************
 	w := httptest.NewRecorder() // creates ResponseRecorder
 
 	wh, err := DBReadAllWebhooks(w) // reads all webhooks from database
@@ -101,7 +48,14 @@ func TestHandlerWebhooksDelete(t *testing.T) {
 		t.Error(err)
 	}
 
-	webH := Webhook{Event: "testevent", URL: "www.testurl.com"} // same webhooks as the one i created in POST test
+	resp = ALLMethodWebhook(Get, URL+wh[1].ID, s, t)
+
+	if resp.StatusCode != http.StatusOK { // check that test went ok
+		t.Error(resp.StatusCode)
+	}
+
+	// Test Delete method for endpoint /cravings/webhooks/
+	fmt.Println("testing webhooks DELETE method")
 
 	var temp string
 
@@ -119,60 +73,176 @@ func TestHandlerWebhooksDelete(t *testing.T) {
 	fmt.Println("temp: ", temp)
 
 	tempstruct := Webhook{ID: temp} // creates temp struct to send with request
-	req, _ := json.Marshal(tempstruct)
-	reqTest := bytes.NewReader(req)                                     // convert struct to *Reader
-	r, err := http.NewRequest("DELETE", "/cravings/webhooks/", reqTest) // creates requests
 
-	if err != nil {
-		t.Error(err)
-	}
-
-	w = httptest.NewRecorder() // creates ResponseRecorder
-
-	handler := http.HandlerFunc(HandlerWebhooks) // test handlerWebhooks
-	handler.ServeHTTP(w, r)
-
-	resp := w.Result()
-	defer resp.Body.Close()
+	resp = ALLMethodWebhook(Delete, URL, tempstruct, t)
 
 	if resp.StatusCode != http.StatusOK { // check that test went ok
 		t.Error(resp.StatusCode)
 	}
-
-	fmt.Println("testing webhooks DELETE method")
 }
 
-// func ALLMethodWebhook(m string, url string, s Webhook, t *testing.T) *http.Response {
-
-// 	r, err := http.NewRequest(m, url, nil) // creates request with body
+// func TestHandlerWebhooksPost(t *testing.T) {
+// 	// Test Post method for endpoint /cravings/webhooks/ ******************'
+// 	webH := Webhook{Event: "testevent", URL: "www.testurl.com"} // create a webhook with event and url to send as body
+// 	req, _ := json.Marshal(webH)
+// 	reqTest := bytes.NewReader(req)                                   // convert struct to *Reader
+// 	r, err := http.NewRequest("POST", "/cravings/webhooks/", reqTest) // creats request with body
 
 // 	if err != nil {
 // 		t.Error(err)
 // 	}
 
-// 	if len(s.Event) > 0 {
+// 	w := httptest.NewRecorder() // creates ResponseRecorder
 
-// 		fmt.Println("len > 0")
-
-// 		test := s
-// 		req, _ := json.Marshal(test)
-// 		reqTest := bytes.NewReader(req) // convert struct to *Reader
-
-// 		r, err = http.NewRequest(m, url, reqTest) // creates request with body
-
-// 		if err != nil {
-// 			t.Error(err)
-// 		}
-// 	}
-
-// 	w := httptest.NewRecorder() // creates ResponsRecorder
-
-// 	handler := http.HandlerFunc(HandlerFood) // test handlerFood
+// 	handler := http.HandlerFunc(HandlerWebhooks) // test handlerWebhooks
 // 	handler.ServeHTTP(w, r)
 
 // 	resp := w.Result()
 // 	defer resp.Body.Close()
 
-// 	return resp
+// 	if resp.StatusCode != http.StatusOK { // check that test went ok
+// 		t.Error(resp.StatusCode)
+// 	}
 
+// 	fmt.Println("testeing webhooks POST method")
 // }
+
+// func TestHandlerWebhooksGetA(t *testing.T) {
+// 	// Test Get method for endpoint /cravings/webhooks/ ***************************
+// 	r, err := http.NewRequest("GET", "/cravings/webhooks/", nil) //creates request
+
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	w := httptest.NewRecorder() // creates ResponseRecorder
+
+// 	handler := http.HandlerFunc(HandlerWebhooks) // test handlerWebhooks
+// 	handler.ServeHTTP(w, r)
+
+// 	resp := w.Result()
+// 	defer resp.Body.Close()
+
+// 	if resp.StatusCode != http.StatusOK { // check that test went ok
+// 		t.Error(resp.StatusCode)
+// 	}
+
+// 	fmt.Println("testing webhooks GET method for all webhooks")
+// }
+
+// func TestHandlerWebhooksGetO(t *testing.T) {
+// 	// Test Get method for endpoint /cravings/webhooks/ID **************
+// 	w := httptest.NewRecorder() // creates ResponseRecorder
+
+// 	wh, err := DBReadAllWebhooks(w) // reads all webhooks from database
+
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	r, err := http.NewRequest("GET", "/cravings/webhooks/"+wh[1].ID, nil) // creats request
+
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	w = httptest.NewRecorder() // creates ResponseRecorder
+
+// 	handler := http.HandlerFunc(HandlerWebhooks) // test handlerWebhooks
+// 	handler.ServeHTTP(w, r)
+
+// 	resp := w.Result()
+// 	defer resp.Body.Close()
+
+// 	if resp.StatusCode != http.StatusOK { // check that test went ok
+// 		t.Error(resp.StatusCode)
+// 	}
+
+// 	fmt.Println("testing webhooks GET method for one webhook")
+// }
+
+// func TestHandlerWebhooksDelete(t *testing.T) {
+// 	// Test Delete method for endpoint /cravings/webhooks/ ****************
+// 	w := httptest.NewRecorder() // creates ResponseRecorder
+
+// 	wh, err := DBReadAllWebhooks(w) // reads all webhooks from database
+
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	webH := Webhook{Event: "testevent", URL: "www.testurl.com"} // same webhooks as the one i created in POST test
+
+// 	var temp string
+
+// 	fmt.Println("webH: ", webH.Event)
+
+// 	for i := range wh { // loops throue all webhooks
+// 		fmt.Println("event: ", wh[i].Event)
+
+// 		if wh[i].Event == webH.Event { // check if webhook is same ass the test webhook we made earlyer
+// 			temp = wh[i].ID // sets temp to be the same as the id for temp webhook
+// 			fmt.Println("tempStruct: ", wh[i])
+// 		}
+// 	}
+
+// 	fmt.Println("temp: ", temp)
+
+// 	tempstruct := Webhook{ID: temp} // creates temp struct to send with request
+// 	req, _ := json.Marshal(tempstruct)
+// 	reqTest := bytes.NewReader(req)                                     // convert struct to *Reader
+// 	r, err := http.NewRequest("DELETE", "/cravings/webhooks/", reqTest) // creates requests
+
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	w = httptest.NewRecorder() // creates ResponseRecorder
+
+// 	handler := http.HandlerFunc(HandlerWebhooks) // test handlerWebhooks
+// 	handler.ServeHTTP(w, r)
+
+// 	resp := w.Result()
+// 	defer resp.Body.Close()
+
+// 	if resp.StatusCode != http.StatusOK { // check that test went ok
+// 		t.Error(resp.StatusCode)
+// 	}
+
+// 	fmt.Println("testing webhooks DELETE method")
+// }
+
+func ALLMethodWebhook(m string, url string, s Webhook, t *testing.T) *http.Response {
+
+	r, err := http.NewRequest(m, url, nil) // creates request with body
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(s.Event) > 0 {
+
+		fmt.Println("len > 0")
+
+		test := s
+		req, _ := json.Marshal(test)
+		reqTest := bytes.NewReader(req) // convert struct to *Reader
+
+		r, err = http.NewRequest(m, url, reqTest) // creates request with body
+
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	w := httptest.NewRecorder() // creates ResponsRecorder
+
+	handler := http.HandlerFunc(HandlerFood) // test handlerFood
+	handler.ServeHTTP(w, r)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	return resp
+
+}
